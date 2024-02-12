b22fbef3b03f3af46191f1e7ed5709ed8dfa9e1f
navaneethsn <navaneethsn@LAP-2480>
2024-02-12 18:16:36 +0530

    Add project files.

---
+++ GenericQueueAssmt/GenericQueue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericQueueAssmt
+{
+    internal class GenericQueue<T> 
+    {
+        private T[] queue;
+        private int head = -1;
+        private int tail = -1;
+        private int capacity;
+
+        public GenericQueue(int capacity)
+        {
+            this.capacity = capacity;
+            queue = new T[capacity];
+        }
+
+        public void enqueue(T item)
+        {
+            if (tail + 1 == capacity)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                if (head == -1 && tail == -1)
+                {
+                    head = 0;
+                    tail = 0;
+                }
+                else
+                {
+                    tail = tail + 1;
+                }
+                queue[tail] = item;
+            }
+        }
+
+
+        public T dequeue()
+        {
+            T Item;
+            if (head > tail || tail == -1)
+            {
+                Console.WriteLine("Queue is Empty");
+                throw new Exception("Empty");
+            }
+            else
+            {
+                Item = queue[head];
+                if (head == tail)
+                {
+                    tail = -1;
+                    head = -1;
+                }
+                else
+                {
+
+
+                    head = head + 1;
+
+                }
+                return Item;
+            }
+        }
+
+
+        public bool IsEmpty()
+        {
+            return head == tail;
+        }
+
+        public bool IsFull()
+        {
+            return head > tail;
+        }
+    }
+}
---
+++ GenericQueueAssmt/Program.cs
@@ -0,0 +1,55 @@
+﻿using GenericQueueAssmt;
+
+GenericQueue<Chat> gqueue = new GenericQueue<Chat>(10);
+
+int msgId = 0;
+bool run = true;
+
+while (run)
+{
+    Console.WriteLine("Queue Operations:");
+    Console.WriteLine("1. Enqueue");
+    Console.WriteLine("2. Dequeue");
+    Console.WriteLine("3. Check if Empty");
+    Console.WriteLine("4. Check if Full");
+    Console.WriteLine("5. Exit");
+    Console.Write("Input: ");
+
+    var inp = Convert.ToInt32(Console.ReadLine());
+
+    switch (inp)
+    {
+        case 1:
+            Console.WriteLine("Enter message");
+            string content = Console.ReadLine();
+            gqueue.enqueue(new Chat
+            {
+                MessageId = ++msgId,
+                Content = content,
+                TimeStamp = DateTime.Now,
+                SourceId = msgId
+            });
+            Console.WriteLine("ChatMessage enqueued.");
+            break;
+        case 2:
+            Chat dequeuedMessage = gqueue.dequeue();
+            if (dequeuedMessage == null)
+            {
+                Console.WriteLine("No message in queue");
+                break;
+            }
+            Console.WriteLine($"Dequeued message: ID {dequeuedMessage.MessageId}, Content: {dequeuedMessage.Content}, TimeStamp: {dequeuedMessage.TimeStamp}, SourceId: {dequeuedMessage.SourceId}");
+            break;
+        case 3:
+            Console.WriteLine(gqueue.IsEmpty());
+            break;
+        case 4:
+            Console.WriteLine(gqueue.IsFull());
+            break;
+        case 5:
+            return;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+}
